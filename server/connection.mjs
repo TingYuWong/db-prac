5fff21cbb2f66aaecb4931c78b51f1c4204f98da
tina <sandy20615@gmail.com>
2024-09-27 15:31:21 +0800

    feat: lint & formatting

--- server/connection.mjs
+++ server/connection.mjs
@@ -1,15 +1,15 @@
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
 
-dotenv.config()
-const client = new MongoClient(process.env.DB_CONNECTION)
+dotenv.config();
+const client = new MongoClient(process.env.DB_CONNECTION);
 
 let conn;
 try {
-    conn = await client.connect()
+  conn = await client.connect();
 } catch {
-    throw new Error('DB Connection Failed.')
+  throw new Error('DB Connection Failed.');
 }
 
-let db = conn.db('adoption')
-export default db
+let db = conn.db('adoption');
+export default db;
--- server/server.mjs
+++ server/server.mjs
@@ -1,23 +1,23 @@
 import express from 'express';
 import db from './connection.mjs';
 
-const PORT = 3000
-const app = express()
+const PORT = 3000;
+const app = express();
 
-app.use(express.static('../static'))
+app.use(express.static('../static'));
 
 app.get('/search/:query', async (req, res) => {
+  let collection = db.collection('pets');
 
-    let collection = db.collection('pets');
-
-    let result = await collection
+  let result = await collection
     .find({ $text: { $search: req.params.query } })
-    .sort({ score: { $meta: "textScore" } })
-    .limit(50).toArray()
+    .sort({ score: { $meta: 'textScore' } })
+    .limit(50)
+    .toArray();
 
-    res.send(result).status(200)
-})
+  res.send(result).status(200);
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+  console.log(`Listening on port ${PORT}`);
+});
